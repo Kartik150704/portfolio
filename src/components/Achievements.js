a8ba79d362d5e043c7c0a1446bde8d145dd0ae17
Kartik150704 <108309065+Kartik150704@users.noreply.github.com>
2023-06-30 00:08:37 +0530

    sss

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -4,6 +4,7 @@ import './Achievements.css';
 const Achievements = () => {
 
     const [ratings, setratings] = useState("Fetching....")
+    const [ranking,setrankings]=useState("Fetching...")
     useEffect(() => {
         fetch(`https://codeforces.com/api/user.rating?handle=kartik150704`)
             .then(response => response.json())
@@ -12,15 +13,27 @@ const Achievements = () => {
                 data=data.result;
                 data=data[data.length-1]
                 data=data.newRating
-                setratings(data)
+                setratings(parseInt(data))
             })
             .catch(error => {
                 // Handle any errors
                 console.error(error);
             });
+            if(ratings>=1200 && ratings<1400)
+            {
+                setrankings("Pupil")
+            }
+            else if(ratings>=1400 && ratings<1600)
+            {
+                setrankings("Specialist")
+            }
+            else if(ratings>=1600)
+            {
+                setrankings("Expert")
+            }
     })
     const achievements = [
-        { title: 'Specialist on Codeforces', content: `Ratings :${ratings} \n` },
+        { title: ranking, content: `Ratings :${ratings} \n` },
         { title: 'JEE Advanced Rank', content: 'AIR - 5389' },
         { title: 'JEE Mains Rank', content: 'AIR -2402' },
         { title: 'Coordinator of Coding Club', content: 'Organized coding events' },
